ec289361abb82e3b60bdb14a6f5b2462df07c1ae
Daniel Li <lidaniel225@gmail.com>
2020-10-17 18:51:45 -0700

    successfully adds to MongoDB database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const { response } = require('express')
 const express = require('express')
 const app = express()
-// const Person = require('./models/person')
+const Request = require('./models/request')
 
 
 const mongoose = require('mongoose')
@@ -15,14 +15,32 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.static('build'))
 
+const url = process.env.MONGODB_URI
 
-app.post('/request', (request, response) => {
-    console.log('asdf')
-    const body = request.body
-    response.json(body)
+app.post('/request', (req, res) => {
+    const body = req.body
+
+    const request = new Request({
+        firstName: body.firstName,
+        lastName: body.lastName,
+        school: body.school,
+        laptops: body.laptops,
+        cost: body.cost,
+        city: body.city,
+        state: body.state,
+        country: body.country,
+        email: body.email,
+        number: body.number,
+        description: body.description
+    })
+
+    request.save().then(savedRequest => {
+        res.json(savedRequest)
+    })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
---
+++ models/request.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const requestSchema = new mongoose.Schema({
+    id: Number,
+    firstName: String,
+    lastName: String,
+    school: String,
+    laptops: String,
+    cost: String,
+    city: String,
+    state: String,
+    country: String,
+    email: String,
+    number: String,
+    description: String
+})
+
+requestSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Request', requestSchema)
